c3fd96e9820ff8aa251500b1db3b2d1cc7ad6d5f
Egor Iudin <iudinegorinfo@gmail.com>
2021-11-10 18:41:58 +0300

    Cleaned and configured project

---
+++ env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.24.06/x86_64-centos7-gcc48-opt/bin/thisroot.sh
+source  /cvmfs/sft.cern.ch/lcg/external/gcc/4.8.4/x86_64-slc6-gcc48-opt/setup.sh /cvmfs/sft.cern.ch/lcg/contrib
+
+export PATH=`pwd`/bin:$PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/cvmfs/sft.cern.ch/lcg/external/Boost/1.53.0_python2.7/x86_64-slc6-gcc48-opt/lib/
+

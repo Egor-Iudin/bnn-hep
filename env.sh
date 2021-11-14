777d20832a84eb1d0b1d7bf376659c18306b70b1
Egor Iudin <iudinegorinfo@gmail.com>
2021-11-14 19:39:04 +0300

    fixed env script

--- env.sh
+++ env.sh
@@ -4,4 +4,4 @@ source  /cvmfs/sft.cern.ch/lcg/external/gcc/4.8.4/x86_64-slc6-gcc48-opt/setup.sh
 
 export PATH=`pwd`/bin:$PATH
 export BOOST_LIBRARY_PATH=/cvmfs/sft.cern.ch/lcg/external/Boost/1.53.0_python2.7/x86_64-slc6-gcc48-opt/
-export BOOST_VERSION="1-53"
+export BOOST_VERSION="1_53"

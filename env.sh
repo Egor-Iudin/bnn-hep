948cc8f7226816d8d470e74e23807be8673cfc79
Egor Iudin <iudinegorinfo@gmail.com>
2021-11-14 19:15:55 +0300

    fixed makefiles env paths

--- env.sh
+++ env.sh
@@ -3,5 +3,5 @@ source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.24.06/x86_64-centos7-gcc48-opt
 source  /cvmfs/sft.cern.ch/lcg/external/gcc/4.8.4/x86_64-slc6-gcc48-opt/setup.sh /cvmfs/sft.cern.ch/lcg/contrib
 
 export PATH=`pwd`/bin:$PATH
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/cvmfs/sft.cern.ch/lcg/external/Boost/1.53.0_python2.7/x86_64-slc6-gcc48-opt/lib/
-
+export BOOST_LIBRARY_PATH=/cvmfs/sft.cern.ch/lcg/external/Boost/1.53.0_python2.7/x86_64-slc6-gcc48-opt/
+export BOOST_VERSION="1-53"

e41af9e942f21325631e6923c8887c70522ad88d
Egor Iudin <iudinegorinfo@gmail.com>
2021-11-24 12:51:40 +0300

    updated env paths

--- env.sh
+++ env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+source /cvmfs/cms.cern.ch/slc7_amd64_gcc900/external/gcc/9.3.0/etc/profile.d/init.sh
 source /cvmfs/cms.cern.ch/slc7_amd64_gcc900/lcg/root/6.22.08/bin/thisroot.sh
-source /cvmfs/sft.cern.ch/lcg/external/gcc/4.8.4/x86_64-slc6-gcc48-opt/setup.sh /cvmfs/sft.cern.ch/lcg/contrib
 
 export PATH=`pwd`/bin:$PATH
 
 export BOOST_LIBRARY_PATH=/cvmfs/cms.cern.ch/slc7_amd64_gcc900/external/boost/1.75.0/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"$BOOST_LIBRARY_PATH"lib/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${BOOST_LIBRARY_PATH}lib/

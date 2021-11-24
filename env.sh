d83be6461dcbbec9d1fe1c0d91bd36a6899063bb
Egor Iudin <iudinegorinfo@gmail.com>
2021-11-24 13:48:12 +0300

    fixed tbb paths

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source /cvmfs/cms.cern.ch/slc7_amd64_gcc900/external/gcc/9.3.0/etc/profile.d/init.sh
 source /cvmfs/cms.cern.ch/slc7_amd64_gcc900/lcg/root/6.22.08/bin/thisroot.sh
-source /cvmfs/cms.cern.ch/slc7_amd64_gcc900/external/tbb/v2021.3.0/etc/profile.d/init.sh
+source /cvmfs/cms.cern.ch/slc7_amd64_gcc900/external/tbb/2020_U3/etc/profile.d/init.sh
 
 
 export PATH=`pwd`/bin:$PATH

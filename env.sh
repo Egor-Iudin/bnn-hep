b8c99853774037fda9111935584e2d1a9e187bc9
Egor Iudin <iudinegorinfo@gmail.com>
2021-11-24 13:41:16 +0300

    added tbb paths

--- env.sh
+++ env.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-source /cvmfs/cms.cern.ch/slc7_amd64_gcc700/external/gcc/7.0.0/etc/profile.d/init.sh
-source /cvmfs/cms.cern.ch/slc7_amd64_gcc700/lcg/root/6.14.09/bin/thisroot.sh
+source /cvmfs/cms.cern.ch/slc7_amd64_gcc900/external/gcc/9.3.0/etc/profile.d/init.sh
+source /cvmfs/cms.cern.ch/slc7_amd64_gcc900/lcg/root/6.22.08/bin/thisroot.sh
+source /cvmfs/cms.cern.ch/slc7_amd64_gcc900/external/tbb/v2021.3.0/etc/profile.d/init.sh
+
 
 export PATH=`pwd`/bin:$PATH
 
-export BOOST_LIBRARY_PATH=/cvmfs/cms.cern.ch/slc7_amd64_gcc700/external/boost/1.67.0/
+export BOOST_LIBRARY_PATH=/cvmfs/cms.cern.ch/slc7_amd64_gcc900/external/boost/1.75.0/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${BOOST_LIBRARY_PATH}lib/

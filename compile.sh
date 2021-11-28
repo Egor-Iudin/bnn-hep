aa6e1ecd767aaa743f2881470d4c606780bce831
Egor Iudin <iudinegorinfo@gmail.com>
2021-11-28 22:33:16 +0300

    removed old files

--- compile.sh
+++
@@ -1,9 +0,0 @@
-rm libCROOT.a ;
-
-gcc -O -pthread -m64 -I/cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.24.06/x86_64-centos7-gcc48-opt/root/include -c CTGraph.cxx -o CTGraph.o;
-ar -cvq libCROOT.a  CTGraph.o;
-gcc -L/cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.24.06/x86_64-centos7-gcc48-opt/root/lib -lCore -lCint -lRIO -lNet -lHist -lGraf -lGraf3d -lGpad -lTree -lRint -lPostscript -lMatrix -lPhysics -lMathCore -lThread -pthread -lm -ldl -rdynamic printError.cxx libCROOT.a  libCROOT.a -lm -o printError;
-
-
-
-
